0bd7e02782bd68cf6ce89f9c17dc67eb53546706
beom <beommaker@gmail.com>
2018-05-28 18:44:24 +0900

    feat(blog): add admins for category and post
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/admin.py
+++ beomlog/blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_editable = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'title', 'created']
+    list_filter = ['category']
+    list_editable = ['category']
+    list_display_links = ['id', 'title']
+    search_fields = ['title', 'content']
+    ordering = ['-created']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+
+
