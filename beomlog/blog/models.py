99cac3d5aee942aa6b0d2b80defda179b846c3d2
beom <beommaker@gmail.com>
2018-05-28 18:43:58 +0900

    refactor(blog): delete verbose name for cleaner admin
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/models.py
+++ beomlog/blog/models.py
@@ -3,10 +3,9 @@ from django.db import models
 
 class Category(models.Model):
 
-    name = models.CharField(verbose_name='이름', max_length=50)
+    name = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name = '분류'
         ordering = ['name']
 
     def __str__(self):
@@ -15,13 +14,12 @@ class Category(models.Model):
 
 class Post(models.Model):
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='분류', null=True, blank=True)
-    title = models.CharField(verbose_name='제목', max_length=256)
-    content = models.TextField(verbose_name='내용', blank=True, default='')
-    created = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=256)
+    content = models.TextField(blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name = '글'
         ordering = ['created']
 
     def __str__(self):
