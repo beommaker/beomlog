5938ceed0b27a99e86256afe164045349124eb1a
beom <beommaker@gmail.com>
2018-05-28 18:19:20 +0900

    feat(blog): add models for category and post
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/models.py
+++ beomlog/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(verbose_name='이름', max_length=50)
+
+    class Meta:
+        verbose_name = '분류'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='분류', null=True, blank=True)
+    title = models.CharField(verbose_name='제목', max_length=256)
+    content = models.TextField(verbose_name='내용', blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
+
+    class Meta:
+        verbose_name = '글'
+        ordering = ['created']
+
+    def __str__(self):
+        return self.title
