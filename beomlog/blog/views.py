a3c0d6f0a9f3a5374b386c6bd0c70fe05e51b0c6
beom <beommaker@gmail.com>
2018-05-29 17:34:05 +0900

    feat(templates): add-post-list
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/views.py
+++ beomlog/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from beomlog.blog.models import Post
+
 
 def home(request):
     template_name = 'home.html'
-    return render(request, template_name)
+    posts = Post.objects.all()
+    context_data = {'object_list': posts}
+
+    return render(request, template_name, context_data)
