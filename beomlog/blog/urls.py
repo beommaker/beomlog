738728552e6dc0f7235c98cb92cff23c8d55a7d7
beom <beommaker@gmail.com>
2018-05-29 19:38:56 +0900

    refactor(blog.views): change fbv to cbv
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/urls.py
+++ beomlog/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from . import views
+
+from beomlog.blog.views import PostListView, PostDetailView
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('<int:post_id>/', views.detail, name='detail'),
+    path('', PostListView.as_view(), name='home'),
+    path('<int:pk>/', PostDetailView.as_view(), name='detail'),
 ]
