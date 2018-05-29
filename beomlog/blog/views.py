f66ddc4ff246b208c241787bdce05fd939e9d6d8
beom <beommaker@gmail.com>
2018-05-29 23:59:13 +0900

    feat(blog): add sorting categories function
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/views.py
+++ beomlog/blog/views.py
@@ -13,6 +13,13 @@ class PostListView(ListView):
         context['categories'] = categories
         return context
 
+    def get_queryset(self):
+        queryset = super(PostListView, self).get_queryset()
+        category_id = self.request.GET.get('category')
+        if category_id:
+            queryset = queryset.filter(category=category_id)
+        return queryset
+
 
 class PostDetailView(DetailView):
     model = Post
