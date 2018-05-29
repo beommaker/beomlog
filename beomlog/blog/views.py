5dd9161ee340f08ee5981a45f64a9fb369817045
beom <beommaker@gmail.com>
2018-05-29 23:51:16 +0900

    feat(blog): add categoy list in home.html
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/views.py
+++ beomlog/blog/views.py
@@ -1,11 +1,17 @@
 from django.views.generic import ListView, DetailView, CreateView
 
-from beomlog.blog.models import Post
+from beomlog.blog.models import Post, Category
 
 
 class PostListView(ListView):
     model = Post
     template_name = 'home.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context['categories'] = categories
+        return context
 
 
 class PostDetailView(DetailView):
