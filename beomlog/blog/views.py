3a2a8d6184bb7f1d404e2fc01762d013c7321f24
beom <beommaker@gmail.com>
2018-05-29 18:00:11 +0900

    feat(blog): add detail page
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/views.py
+++ beomlog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from beomlog.blog.models import Post
 
@@ -8,4 +8,11 @@ def home(request):
     posts = Post.objects.all()
     context_data = {'object_list': posts}
 
+    return render(request, template_name, context_data)
+
+
+def detail(request, post_id):
+    template_name = 'detail.html'
+    post = get_object_or_404(Post, id=post_id)
+    context_data = {'object': post}
     return render(request, template_name, context_data)
