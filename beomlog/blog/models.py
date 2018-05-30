ca81a4889a22471ed50d65c95a8f1264cd16eec2
beom <beommaker@gmail.com>
2018-05-30 18:50:17 +0900

    feat(blog): add delete function for media folder
    https://trello.com/c/XBOGu2XY

--- beomlog/blog/models.py
+++ beomlog/blog/models.py
@@ -33,3 +33,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.id})
 
+    def delete(self):
+        os.remove(self.image.path)
+        return super(Post, self).delete()
